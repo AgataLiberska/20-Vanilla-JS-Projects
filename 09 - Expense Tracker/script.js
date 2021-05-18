7e9562543b0bb99d0c597fe893640b3711deb7a1
Agata Liberska <liberska@gmail.com>
2021-05-18 21:09:20 +0100

    add variables and dummy data

---
+++ 09 - Expense Tracker/script.js
@@ -0,0 +1,14 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+];
