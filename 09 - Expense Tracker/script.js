c9b5392ad483b624a8dc462007e8334af13750ea
Agata Liberska <liberska@gmail.com>
2021-05-18 21:17:02 +0100

    add transactions to DOM

--- 09 - Expense Tracker/script.js
+++ 09 - Expense Tracker/script.js
@@ -12,3 +12,33 @@ const dummyTransactions = [
     { id: 3, text: 'Book', amount: -10 },
     { id: 4, text: 'Camera', amount: 150 }
 ];
+
+let transactions = dummyTransactions;
+
+//Add transactions to DOM list
+function addTransactionDOM(transaction) {
+    // get sign
+    const sign = transaction.amount > 0 ? '+' : '-';
+
+    const item = document.createElement('li');
+    
+    // add class based on value 
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+        ${transaction.text} <span>${sign}$${Math.abs(transaction.amount)}</span><button class="delete-btn">X</button>
+    `;
+
+    list.appendChild(item);
+}
+
+// init app
+function init() {
+    list.innerHTML = '';
+
+    transactions.forEach(transaction => {
+        addTransactionDOM(transaction);
+    })
+}
+
+init();
